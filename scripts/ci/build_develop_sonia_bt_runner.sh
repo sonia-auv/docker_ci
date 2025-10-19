f910ae067f31b33561a5fc6e7f2614685e50da42
ArthurPV <arthurpontiervalois@gmail.com>
2025-10-18 20:56:57 -0400

    Prepare the merge

--- scripts/ci/build_develop_sonia_bt_runner.sh
+++ scripts/ci/build_develop_sonia_bt_runner.sh
@@ -3,5 +3,4 @@
 set -e
 set -o pipefail
 
-# TODO: Let the branch name empty
-docker build --build-arg BUILD_FOR=sonia_bt_runner --build-arg BRANCH_NAME=add-ci-cd .
+docker build --build-arg BUILD_FOR=sonia_bt_runner .
--- scripts/ci/build_develop_sonia_common_ros2.sh
+++ scripts/ci/build_develop_sonia_common_ros2.sh
@@ -3,5 +3,4 @@
 set -e
 set -o pipefail
 
-# TODO: Let the branch name empty.
-docker build --build-arg BUILD_FOR=sonia_common_ros2 --build-arg BRANCH_NAME=add-ci-cd .
+docker build --build-arg BUILD_FOR=sonia_common_ros2 .
